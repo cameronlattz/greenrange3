f4d04927b8dea32bd68536cd2067bb6465f99c78
Elianice Gorniak <eligorniak@gmail.com>
2019-10-24 13:03:28 -0500

    deleted extra files in LandingPage

--- client/src/components/LandingPage/HowItWorks/index.js
+++ client/src/components/LandingPage/HowItWorks/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import imageLogo from "../images/green-range-logo12.8.png";
+import imagehowitworks from "./howitworks.jpg";
 import "./style.css";
 
 const useStyles = makeStyles(theme => ({
@@ -14,18 +12,6 @@ const useStyles = makeStyles(theme => ({
     width: "250px",
     alignItems: "center"
   },
-  subtitle: {
-    fontSize: "18px",
-    padding: "10px",
-    fontStyle: "italic",
-    fontFamily: "Didact Gothic"
-  },
-  p: {
-    fontSize: "15px",
-    padding: "10px",
-    fontStyle: "italic",
-    fontFamily: "Didact Gothic"
-  },
   title: {
     fontSize: "30px",
     padding: "10px",
@@ -35,50 +21,17 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Testimonials() {
+export default function HowItWorks() {
   const classes = useStyles();
   return (
     <div className="container">
-      <h1 className={classes.title}>What our customers are saying about us!</h1>
+      <h1 className={classes.title}>How it works</h1>
       <div className="container-card">
-        <div className="comment-card">
-          <Paper className={classes.root}>
-            <img src={imageLogo} alt="Logo" style={{ width: 30, height: 30 }} />
-            <Typography variant="h6" className={classes.subtitle}>
-              "Green Range is on my side every week. It's simple, easy to use
-              and the meal suggestions are amazing.”
-            </Typography>
-
-            <Typography component="p" className={classes.p}>
-              Emmanuelle Smith - Minneapolis
-            </Typography>
-          </Paper>
-        </div>
-        <div className="comment-card">
-          <Paper className={classes.root}>
-            <img src={imageLogo} alt="Logo" style={{ width: 30, height: 30 }} />
-            <Typography variant="h6" className={classes.subtitle}>
-              "I love to come home and start to cooking with my husband. Green
-              Range made our dinners more enjoyable.”
-            </Typography>
-            <Typography component="p" className={classes.p}>
-              Cassia Johnson - Duluth
-            </Typography>
-          </Paper>
-        </div>
-        <div className="comment-card">
-          <Paper className={classes.root}>
-            <img src={imageLogo} alt="Logo" style={{ width: 30, height: 30 }} />
-            <Typography variant="h6" className={classes.subtitle}>
-              "My favorite thing in Green Range are the freshness of the
-              ingredients and how fast is the delivery.”
-            </Typography>
-
-            <Typography component="p" className={classes.p}>
-              Richard Swanson - Eagan
-            </Typography>
-          </Paper>
-        </div>
+        <img
+          src={imagehowitworks}
+          alt="How it works"
+          style={{ width: "auto", height: "500px" }}
+        />
       </div>
     </div>
   );
